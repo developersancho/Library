27457c105a2ce2d77b69cb4678da1edb8dc69dcb
developersancho <developersancho@gmail.com>
2018-02-09 16:35:15 +0300

    ----

---
+++ LibraryData/ICheckout.cs
@@ -0,0 +1,30 @@
+﻿using LibraryData.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LibraryData
+{
+    public interface ICheckout
+    {
+        IEnumerable<CheckOut> GetAll();
+        CheckOut Get(int id);
+        void Add(CheckOut newCheckout);
+        IEnumerable<CheckoutHistory> GetCheckoutHistory(int id);
+        void PlaceHold(int assetId, int libraryCardId);
+        void CheckoutItem(int assetId, int libraryCardId);
+        void CheckInItem(int assetId);
+        CheckOut GetLatestCheckout(int id);
+        int GetNumberOfCopies(int id);
+        int GetAvailableCopies(int id);
+        bool IsCheckedOut(int id);
+
+        string GetCurrentHoldPatronName(int id);
+        string GetCurrentHoldPlaced(int id);
+        string GetCurrentCheckoutPatron(int id);
+        IEnumerable<Hold> GetCurrentHolds(int id);
+
+        void MarkLost(int id);
+        void MarkFound(int id);
+    }
+}
